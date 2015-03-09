8d1d9348ff71f2e449c272682d662b09b5644e0e
Siddharth Bhat <siddu.druid@gmail.com>
2015-03-10 00:18:21 +0530

    fix flake8 style in galaxy_specrend.py

--- examples/demo/galaxy/galaxy_specrend.py
+++ examples/demo/galaxy/galaxy_specrend.py
@@ -45,46 +45,55 @@ import math
 """
 GAMMA_REC709 = 0
 
-NTSCsystem  =  {"name": "NTSC",
-    "xRed": 0.67, "yRed": 0.33,
-    "xGreen": 0.21, "yGreen": 0.71,
-    "xBlue": 0.14, "yBlue": 0.08,
-    "xWhite": 0.3101, "yWhite": 0.3163, "gamma": GAMMA_REC709}
-EBUsystem  =  {"name": "SUBU (PAL/SECAM)",
-    "xRed": 0.64, "yRed": 0.33,
-    "xGreen": 0.29, "yGreen": 0.60,
-    "xBlue": 0.15, "yBlue": 0.06,
-    "xWhite": 0.3127, "yWhite": 0.3291, "gamma": GAMMA_REC709 }
-SMPTEsystem  =  {"name": "SMPTE",
-    "xRed": 0.63, "yRed": 0.34,
-    "xGreen": 0.31, "yGreen": 0.595,
-    "xBlue": 0.155, "yBlue": 0.07,
-    "xWhite": 0.3127, "yWhite": 0.3291, "gamma": GAMMA_REC709 }
-HDTVsystem  =  {"name": "HDTV",
-    "xRed": 0.67, "yRed": 0.33,
-    "xGreen": 0.21, "yGreen": 0.71,
-    "xBlue": 0.15, "yBlue": 0.06,
-    "xWhite": 0.3127, "yWhite": 0.3291, "gamma": GAMMA_REC709 }
-CIEsystem  =  {"name": "CIE",
-    "xRed": 0.7355, "yRed": 0.2645,
-    "xGreen": 0.2658, "yGreen": 0.7243,
-    "xBlue": 0.1669, "yBlue": 0.0085,
-    "xWhite": 0.3333333333, "yWhite": 0.3333333333, "gamma": GAMMA_REC709 }
-Rec709system  =  {"name": "CIE REC709",
-    "xRed": 0.64, "yRed": 0.33,
-    "xGreen": 0.30, "yGreen": 0.60,
-    "xBlue": 0.15, "yBlue": 0.06,
-    "xWhite": 0.3127, "yWhite": 0.3291, "gamma": GAMMA_REC709 }
+NTSCsystem = {"name": "NTSC",
+              "xRed": 0.67, "yRed": 0.33,
+              "xGreen": 0.21, "yGreen": 0.71,
+              "xBlue": 0.14, "yBlue": 0.08,
+              "xWhite": 0.3101, "yWhite": 0.3163, "gamma": GAMMA_REC709}
+
+EBUsystem = {"name": "SUBU (PAL/SECAM)",
+             "xRed": 0.64, "yRed": 0.33,
+             "xGreen": 0.29, "yGreen": 0.60,
+             "xBlue": 0.15, "yBlue": 0.06,
+             "xWhite": 0.3127, "yWhite": 0.3291, "gamma": GAMMA_REC709}
+SMPTEsystem = {"name": "SMPTE",
+               "xRed": 0.63, "yRed": 0.34,
+               "xGreen": 0.31, "yGreen": 0.595,
+               "xBlue": 0.155, "yBlue": 0.07,
+               "xWhite": 0.3127, "yWhite": 0.3291, "gamma": GAMMA_REC709}
+
+HDTVsystem = {"name": "HDTV",
+              "xRed": 0.67, "yRed": 0.33,
+              "xGreen": 0.21, "yGreen": 0.71,
+              "xBlue": 0.15, "yBlue": 0.06,
+              "xWhite": 0.3127, "yWhite": 0.3291, "gamma": GAMMA_REC709}
+
+CIEsystem = {"name": "CIE",
+             "xRed": 0.7355, "yRed": 0.2645,
+             "xGreen": 0.2658, "yGreen": 0.7243,
+             "xBlue": 0.1669, "yBlue": 0.0085,
+             "xWhite": 0.3333333333, "yWhite": 0.3333333333,
+             "gamma": GAMMA_REC709}
+
+Rec709system = {"name": "CIE REC709",
+                "xRed": 0.64, "yRed": 0.33,
+                "xGreen": 0.30, "yGreen": 0.60,
+                "xBlue": 0.15, "yBlue": 0.06,
+                "xWhite": 0.3127, "yWhite": 0.3291,
+                "gamma": GAMMA_REC709}
+
 
 def upvp_to_xy(up, vp):
     xc = (9 * up) / ((6 * up) - (16 * vp) + 12)
     yc = (4 * vp) / ((6 * up) - (16 * vp) + 12)
     return(xc, yc)
 
+
 def xy_toupvp(xc, yc):
-    up = (4 * xc) / ((-2 * xc) + (12 * yc) + 3);
-    vp = (9 * yc) / ((-2 * xc) + (12 * yc) + 3);
+    up = (4 * xc) / ((-2 * xc) + (12 * yc) + 3)
+    vp = (9 * yc) / ((-2 * xc) + (12 * yc) + 3)
     return(up, vp)
+
 
 def xyz_to_rgb(cs, xc, yc, zc):
     """
@@ -130,14 +139,21 @@ def xyz_to_rgb(cs, xc, yc, zc):
     gw = ((gx * xw) + (gy * yw) + (gz * zw)) / yw
     bw = ((bx * xw) + (by * yw) + (bz * zw)) / yw
 
-    rx = rx / rw;  ry = ry / rw;  rz = rz / rw
-    gx = gx / gw;  gy = gy / gw;  gz = gz / gw
-    bx = bx / bw;  by = by / bw;  bz = bz / bw
+    rx = rx / rw
+    ry = ry / rw
+    rz = rz / rw
+    gx = gx / gw
+    gy = gy / gw
+    gz = gz / gw
+    bx = bx / bw
+    by = by / bw
+    bz = bz / bw
 
     r = (rx * xc) + (ry * yc) + (rz * zc)
     g = (gx * xc) + (gy * yc) + (gz * zc)
     b = (bx * xc) + (by * yc) + (bz * zc)
-    return(r,g,b)
+
+    return(r, g, b)
 
 
 def inside_gamut(r, g, b):
@@ -160,14 +176,15 @@ def constrain_rgb(r, g, b):
     components were modified, zero otherwise.
     """
     # Amount of white needed is w = - min(0, *r, *g, *b)
-    w = -min([0, r, g, b]) # I think?
+    w = -min([0, r, g, b])  # I think?
 
     # Add just enough white to make r, g, b all positive.
     if w > 0:
         r += w
         g += w
         b += w
-    return(r,g,b)
+    return(r, g, b)
+
 
 def gamma_correct(cs, c):
     """
@@ -192,11 +209,13 @@ def gamma_correct(cs, c):
         c = math.pow(c, 1.0 / gamma)
     return(c)
 
+
 def gamma_correct_rgb(cs, r, g, b):
     r = gamma_correct(cs, r)
     g = gamma_correct(cs, g)
     b = gamma_correct(cs, b)
-    return(r,g,b)
+    return (r, g, b)
+
 
 def norm_rgb(r, g, b):
     """
@@ -211,7 +230,9 @@ def norm_rgb(r, g, b):
         b /= greatest
     return(r, g, b)
 
-def spectrum_to_xyz(spec_intens, temp): #spec_intens is a function
+
+# spec_intens is a function
+def spectrum_to_xyz(spec_intens, temp):
     """
     Calculate the CIE X, Y, and Z coordinates corresponding to
     a light source with spectral distribution given by  the
@@ -245,55 +266,113 @@ def spectrum_to_xyz(spec_intens, temp): #spec_intens is a function
     """
 
     cie_colour_match = [
-        [0.0014,0.0000,0.0065], [0.0022,0.0001,0.0105], [0.0042,0.0001,0.0201],
-        [0.0076,0.0002,0.0362], [0.0143,0.0004,0.0679], [0.0232,0.0006,0.1102],
-        [0.0435,0.0012,0.2074], [0.0776,0.0022,0.3713], [0.1344,0.0040,0.6456],
-        [0.2148,0.0073,1.0391], [0.2839,0.0116,1.3856], [0.3285,0.0168,1.6230],
-        [0.3483,0.0230,1.7471], [0.3481,0.0298,1.7826], [0.3362,0.0380,1.7721],
-        [0.3187,0.0480,1.7441], [0.2908,0.0600,1.6692], [0.2511,0.0739,1.5281],
-        [0.1954,0.0910,1.2876], [0.1421,0.1126,1.0419], [0.0956,0.1390,0.8130],
-        [0.0580,0.1693,0.6162], [0.0320,0.2080,0.4652], [0.0147,0.2586,0.3533],
-        [0.0049,0.3230,0.2720], [0.0024,0.4073,0.2123], [0.0093,0.5030,0.1582],
-        [0.0291,0.6082,0.1117], [0.0633,0.7100,0.0782], [0.1096,0.7932,0.0573],
-        [0.1655,0.8620,0.0422], [0.2257,0.9149,0.0298], [0.2904,0.9540,0.0203],
-        [0.3597,0.9803,0.0134], [0.4334,0.9950,0.0087], [0.5121,1.0000,0.0057],
-        [0.5945,0.9950,0.0039], [0.6784,0.9786,0.0027], [0.7621,0.9520,0.0021],
-        [0.8425,0.9154,0.0018], [0.9163,0.8700,0.0017], [0.9786,0.8163,0.0014],
-        [1.0263,0.7570,0.0011], [1.0567,0.6949,0.0010], [1.0622,0.6310,0.0008],
-        [1.0456,0.5668,0.0006], [1.0026,0.5030,0.0003], [0.9384,0.4412,0.0002],
-        [0.8544,0.3810,0.0002], [0.7514,0.3210,0.0001], [0.6424,0.2650,0.0000],
-        [0.5419,0.2170,0.0000], [0.4479,0.1750,0.0000], [0.3608,0.1382,0.0000],
-        [0.2835,0.1070,0.0000], [0.2187,0.0816,0.0000], [0.1649,0.0610,0.0000],
-        [0.1212,0.0446,0.0000], [0.0874,0.0320,0.0000], [0.0636,0.0232,0.0000],
-        [0.0468,0.0170,0.0000], [0.0329,0.0119,0.0000], [0.0227,0.0082,0.0000],
-        [0.0158,0.0057,0.0000], [0.0114,0.0041,0.0000], [0.0081,0.0029,0.0000],
-        [0.0058,0.0021,0.0000], [0.0041,0.0015,0.0000], [0.0029,0.0010,0.0000],
-        [0.0020,0.0007,0.0000], [0.0014,0.0005,0.0000], [0.0010,0.0004,0.0000],
-        [0.0007,0.0002,0.0000], [0.0005,0.0002,0.0000], [0.0003,0.0001,0.0000],
-        [0.0002,0.0001,0.0000], [0.0002,0.0001,0.0000], [0.0001,0.0000,0.0000],
-        [0.0001,0.0000,0.0000], [0.0001,0.0000,0.0000], [0.0000,0.0000,0.0000]]
+        [0.0014, 0.0000, 0.0065],
+        [0.0022, 0.0001, 0.0105],
+        [0.0042, 0.0001, 0.0201],
+        [0.0076, 0.0002, 0.0362],
+        [0.0143, 0.0004, 0.0679],
+        [0.0232, 0.0006, 0.1102],
+        [0.0435, 0.0012, 0.2074],
+        [0.0776, 0.0022, 0.3713],
+        [0.1344, 0.0040, 0.6456],
+        [0.2148, 0.0073, 1.0391],
+        [0.2839, 0.0116, 1.3856],
+        [0.3285, 0.0168, 1.6230],
+        [0.3483, 0.0230, 1.7471],
+        [0.3481, 0.0298, 1.7826],
+        [0.3362, 0.0380, 1.7721],
+        [0.3187, 0.0480, 1.7441],
+        [0.2908, 0.0600, 1.6692],
+        [0.2511, 0.0739, 1.5281],
+        [0.1954, 0.0910, 1.2876],
+        [0.1421, 0.1126, 1.0419],
+        [0.0956, 0.1390, 0.8130],
+        [0.0580, 0.1693, 0.6162],
+        [0.0320, 0.2080, 0.4652],
+        [0.0147, 0.2586, 0.3533],
+        [0.0049, 0.3230, 0.2720],
+        [0.0024, 0.4073, 0.2123],
+        [0.0093, 0.5030, 0.1582],
+        [0.0291, 0.6082, 0.1117],
+        [0.0633, 0.7100, 0.0782],
+        [0.1096, 0.7932, 0.0573],
+        [0.1655, 0.8620, 0.0422],
+        [0.2257, 0.9149, 0.0298],
+        [0.2904, 0.9540, 0.0203],
+        [0.3597, 0.9803, 0.0134],
+        [0.4334, 0.9950, 0.0087],
+        [0.5121, 1.0000, 0.0057],
+        [0.5945, 0.9950, 0.0039],
+        [0.6784, 0.9786, 0.0027],
+        [0.7621, 0.9520, 0.0021],
+        [0.8425, 0.9154, 0.0018],
+        [0.9163, 0.8700, 0.0017],
+        [0.9786, 0.8163, 0.0014],
+        [1.0263, 0.7570, 0.0011],
+        [1.0567, 0.6949, 0.0010],
+        [1.0622, 0.6310, 0.0008],
+        [1.0456, 0.5668, 0.0006],
+        [1.0026, 0.5030, 0.0003],
+        [0.9384, 0.4412, 0.0002],
+        [0.8544, 0.3810, 0.0002],
+        [0.7514, 0.3210, 0.0001],
+        [0.6424, 0.2650, 0.0000],
+        [0.5419, 0.2170, 0.0000],
+        [0.4479, 0.1750, 0.0000],
+        [0.3608, 0.1382, 0.0000],
+        [0.2835, 0.1070, 0.0000],
+        [0.2187, 0.0816, 0.0000],
+        [0.1649, 0.0610, 0.0000],
+        [0.1212, 0.0446, 0.0000],
+        [0.0874, 0.0320, 0.0000],
+        [0.0636, 0.0232, 0.0000],
+        [0.0468, 0.0170, 0.0000],
+        [0.0329, 0.0119, 0.0000],
+        [0.0227, 0.0082, 0.0000],
+        [0.0158, 0.0057, 0.0000],
+        [0.0114, 0.0041, 0.0000],
+        [0.0081, 0.0029, 0.0000],
+        [0.0058, 0.0021, 0.0000],
+        [0.0041, 0.0015, 0.0000],
+        [0.0029, 0.0010, 0.0000],
+        [0.0020, 0.0007, 0.0000],
+        [0.0014, 0.0005, 0.0000],
+        [0.0010, 0.0004, 0.0000],
+        [0.0007, 0.0002, 0.0000],
+        [0.0005, 0.0002, 0.0000],
+        [0.0003, 0.0001, 0.0000],
+        [0.0002, 0.0001, 0.0000],
+        [0.0002, 0.0001, 0.0000],
+        [0.0001, 0.0000, 0.0000],
+        [0.0001, 0.0000, 0.0000],
+        [0.0001, 0.0000, 0.0000],
+        [0.0000, 0.0000, 0.0000]]
 
     X = 0
     Y = 0
     Z = 0
-    for i, lamb in enumerate(range(380, 780, 5)): #lambda = 380; lambda < 780.1; i++, lambda += 5) {
-        Me = spec_intens(lamb, temp);
+    # lambda = 380; lambda < 780.1; i++, lambda += 5) {
+    for i, lamb in enumerate(range(380, 780, 5)):
+        Me = spec_intens(lamb, temp)
         X += Me * cie_colour_match[i][0]
         Y += Me * cie_colour_match[i][1]
         Z += Me * cie_colour_match[i][2]
     XYZ = (X + Y + Z)
-    x = X / XYZ;
-    y = Y / XYZ;
-    z = Z / XYZ;
+    x = X / XYZ
+    y = Y / XYZ
+    z = Z / XYZ
+
     return(x, y, z)
+
 
 def bb_spectrum(wavelength, bbTemp=5000):
     """
     Calculate, by Planck's radiation law, the emittance of a black body
     of temperature bbTemp at the given wavelength (in metres).  */
     """
-    wlm = wavelength * 1e-9 # Convert to metres
-    return (3.74183e-16 * math.pow(wlm, -5.0)) / (math.exp(1.4388e-2 / (wlm * bbTemp)) - 1.0)
+    wlm = wavelength * 1e-9  # Convert to metres
+    return (3.74183e-16 *
+            math.pow(wlm, -5.0)) / (math.exp(1.4388e-2 / (wlm * bbTemp)) - 1.0)
 
     """  Built-in test program which displays the x, y, and Z and RGB
     values for black body spectra from 1000 to 10000 degrees kelvin.
@@ -323,16 +402,17 @@ def bb_spectrum(wavelength, bbTemp=5000):
 """
 
 if __name__ == "__main__":
-    print ("Temperature       x      y      z       R     G     B\n")
-    print ("-----------    ------ ------ ------   ----- ----- -----\n")
+    print("Temperature       x      y      z       R     G     B\n")
+    print("-----------    ------ ------ ------   ----- ----- -----\n")
 
     for t in range(1000, 10000, 500):  # (t = 1000; t <= 10000; t+= 500) {
         x, y, z = spectrum_to_xyz(bb_spectrum, t)
 
         r, g, b = xyz_to_rgb(SMPTEsystem, x, y, z)
 
-        print ("  %5.0f K      %.4f %.4f %.4f   " % (t, x, y, z))
+        print("  %5.0f K      %.4f %.4f %.4f   " % (t, x, y, z))
 
-        r, g, b = constrain_rgb(r, g, b) # I omit the approximation bit here.
+        # I omit the approximation bit here.
+        r, g, b = constrain_rgb(r, g, b)
         r, g, b = norm_rgb(r, g, b)
-        print ("%.3f %.3f %.3f" % (r, g, b))
+        print("%.3f %.3f %.3f" % (r, g, b))
